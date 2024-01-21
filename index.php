d17d1783bc03cc32eae2de473c83e32caebea368
Sara Romao <sara-romao@live.com>
2024-01-20 23:40:59 -0300

    feat: remoção texto dos botoes de ação

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ include("db_config.php");
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Lista Clientes</title>
-     
+     <link rel="stylesheet" href="styles.css">
     
       <!-- bootsrap -->
     <link href='https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css' rel='stylesheet' type='text/css'>
@@ -63,20 +63,20 @@ include("db_config.php");
                     <td><?php echo $email;?></td>
                     <td><?php echo $contato;?></td>
                   
-                    <td class="d-flex ">
-                        <button type="button" data-id="<?php echo $id;?>" class="btn-editar btn btn-success btn-sm mr-3">
+                    <td class="d-flex justify-content-between btn-acoes">
+                        <button type="button" data-id="<?php echo $id;?>" class="btn-editar btn btn-success btn-sm ">
                             
                           <svg width="13" height="13" viewBox="0 0 13 13" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <path d="M7.38215 2.36727L10.6326 5.61783L3.57442 12.6763L0.67641 12.9962C0.288452 13.0391 -0.0393326 12.7111 0.00383034 12.3231L0.326283 9.42293L7.38215 2.36727ZM12.643 1.88332L11.1168 0.357062C10.6407 -0.119021 9.86859 -0.119021 9.39253 0.357062L7.95673 1.79293L11.2072 5.04349L12.643 3.60762C13.119 3.13129 13.119 2.3594 12.643 1.88332Z" fill="white"/>
                           </svg>
-                            Editar
+                           
                         </button>
 
                         <button type="button" data-id="<?php echo $id;?>"  class="btn-excluir btn btn-danger btn-sm">
                             <svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
                                 <path d="M2 2L7 7M12 12L7 7M7 7L2 12L12 2" stroke="white" stroke-width="4" stroke-linecap="round" stroke-linejoin="round"/>
                             </svg>
-                            Remover
+                           
                         </button>
                         
                     </td>
@@ -185,19 +185,19 @@ $(document).ready(function () {
                                 <td>${dataCreate.cliente_nome}</td>
                                 <td>${dataCreate.cliente_email}</td>
                                 <td>${dataCreate.cliente_contato}</td>
-                                <td>
-                                    <button data-id="${idNovoCliente.id}" class='btn btn-info btn-sm btn-edit'>
+                                <td class="d-flex justify-content-between btn-acoes">
+                                    <button data-id="${idNovoCliente.id}" class='btn btn-success btn-sm btn-edit'>
                                       <svg width="13" height="13" viewBox="0 0 13 13" fill="none" xmlns="http://www.w3.org/2000/svg">
                                         <path d="M7.38215 2.36727L10.6326 5.61783L3.57442 12.6763L0.67641 12.9962C0.288452 13.0391 -0.0393326 12.7111 0.00383034 12.3231L0.326283 9.42293L7.38215 2.36727ZM12.643 1.88332L11.1168 0.357062C10.6407 -0.119021 9.86859 -0.119021 9.39253 0.357062L7.95673 1.79293L11.2072 5.04349L12.643 3.60762C13.119 3.13129 13.119 2.3594 12.643 1.88332Z" fill="white"/>
                                       </svg>
-                                      Editar
+                                      
                                     </button>
 
                                     <button data-id="${idNovoCliente.id}" class='btn-excluir btn btn-danger btn-sm'>
-                                    <svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                        <path d="M2 2L7 7M12 12L7 7M7 7L2 12L12 2" stroke="white" stroke-width="4" stroke-linecap="round" stroke-linejoin="round"/>
-                                    </svg>
-                                     Remover
+                                      <svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                          <path d="M2 2L7 7M12 12L7 7M7 7L2 12L12 2" stroke="white" stroke-width="4" stroke-linecap="round" stroke-linejoin="round"/>
+                                      </svg>
+                                      
                                     </button>
                                 </td>
                             </tr>
